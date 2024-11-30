e2e18811fa0eb8a579f2694f1bb42fb126c19f14
Tamil-aruvi <tamilswa98@gmail.com>
2024-11-30 20:10:36 +0530

    first commit

---
+++ src/components/getuserdetail.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Getuserdetail() {
+  return (
+    <div>
+      <h1>Get User</h1>
+    </div>
+  )
+}
+
+export default Getuserdetail
---
+++ src/components/restapi/getapi.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function getapi() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default getapi
